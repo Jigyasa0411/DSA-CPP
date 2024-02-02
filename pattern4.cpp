e55511e79b5ca3a1254476e256b16a76195de0a6
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-02-02 21:15:11 +0530

    Inverted Triangle

--- pattern4.cpp
+++ pattern4.cpp
@@ -4,15 +4,15 @@ int main(){
     for(int row=0;row<3;row++){
         if(row==0||row==2){
             for(int i=0;i<5;i++){
-                cout<<"*";
+                cout<<" *";
             }
         }
         else{
-            cout<<"*";
+            cout<<" *";
             for(int i=0;i<3;i++){
                 cout<<" ";
             }
-            cout<<"*";
+            cout<<" *";
         }
         cout<<endl;
         }
